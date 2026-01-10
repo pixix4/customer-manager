a8dc89715690d9a4bfc576eca26984350a002b8c
Lars Westermann <git@lars-westermann.de>
2026-01-10 15:09:59 +0100

    Add title bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import { createSignal } from "solid-js";
 import styles from "./App.module.css";
 import EmployeeEditDialog from "./employee/EmployeeEditDialog";
+import { RiUserFacesAccountCircleLine } from "solid-icons/ri";
 
 export default function App() {
   const [showEmployeeEditDialog, setShowEmployeeEditDialog] =
-    createSignal(true);
+    createSignal(false);
 
   return (
-    <main>
+    <>
+      <div class={styles.app}>
+        <header data-tauri-drag-region class={styles.titleBar}>
+          <div class={styles.titleBarActions}>
+            <button
+              class={styles.titleBarButton}
+              onClick={() => setShowEmployeeEditDialog((x) => !x)}
+            >
+              <RiUserFacesAccountCircleLine />
+            </button>
+          </div>
+        </header>
+        <main class={styles.content}>Content</main>
+      </div>
+
       <EmployeeEditDialog
         show={showEmployeeEditDialog()}
         setShow={setShowEmployeeEditDialog}
       />
-    </main>
+    </>
   );
 }
